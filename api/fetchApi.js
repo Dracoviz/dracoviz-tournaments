fd92ee093a7209e6e82455fe2a64af3b0cc03f0f
DeveloperKhan <adib.khan@gmail.com>
2023-08-18 04:20:10 -0400

    Add trailing slash to fetch

--- api/fetchApi.js
+++ api/fetchApi.js
@@ -2,7 +2,7 @@ const API_HOST = process.env.API_HOST;
 const API_KEY = process.env.API_KEY;
 
 const fetchApi = (url, method, headers, body) => {
-  return fetch(`${API_HOST}/${url}`, {
+  return fetch(`${API_HOST}/${url}/`, {
     method,
     headers: {
         "x_authorization": `Basic ${API_KEY}`,
