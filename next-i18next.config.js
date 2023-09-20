0b3eedea3df153a46a705f43292885a15cb7fddb
DeveloperKhan <adib.khan@gmail.com>
2023-09-20 11:46:38 -0700

    Fix module import

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,10 @@
-import { resolve } from 'path';
+const path = require('path');
 
 /** @type {import('next-i18next').UserConfig} */
-export const i18n = {
-  defaultLocale: 'es',
-  locales: ['en', 'es'],
-  localePath: resolve('./public/locales'),
-};
+module.exports = {
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['en', 'es'],
+    localePath: path.resolve('./public/locales'),
+  },
+}
