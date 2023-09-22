79b11af1e9af8d532c0438d3d176981ba73dc605
DeveloperKhan <adib.khan@gmail.com>
2023-09-21 20:47:37 -0700

    Fix default language

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
-    defaultLocale: 'es',
+    defaultLocale: 'en',
     locales: ['en', 'es'],
     localePath: path.resolve('./public/locales'),
   },
