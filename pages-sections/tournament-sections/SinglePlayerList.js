86216892f8a4275dd01dfcb392c44455393abb56
DeveloperKhan <adib.khan@gmail.com>
2023-10-01 23:51:59 -0700

    Add win loss count

--- pages-sections/tournament-sections/SinglePlayerList.js
+++ pages-sections/tournament-sections/SinglePlayerList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@mui/styles";
-import { Button } from "@mui/material";
+import { Button, Chip } from "@mui/material";
 import Card from "../../components/Card/Card";
 import styles from "/styles/jss/nextjs-material-kit/sections/singlePlayerStyle.js";
 import CustomInput from "../../components/CustomInput/CustomInput.js";
@@ -10,11 +10,26 @@ import getValidLabel from "../../api/getValidLabel";
 
 const useStyles = makeStyles(styles);
 
+const sortingAlgo = (a, b) => {
+  if (a.wins != null) {
+    return ((b.wins * 10) + b.gameWins) - ((a.wins * 10) + a.gameWins);
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+}
+
 export default function SinglePlayerList(props) {
   const { players, onPlayer, onDeletePlayer, isHost, showValid } = props;
   const { t } = useTranslation();
   const classes = useStyles();
   const [searchedPlayers, setSearchedPlayers] = useState([]);
+
+  console.log(players);
 
   useEffect(() => {
     setSearchedPlayers(players);
@@ -30,6 +45,19 @@ export default function SinglePlayerList(props) {
       return matches;
     });
     setSearchedPlayers(matchingPlayers);
+  }
+
+  const renderStats = (player) => {
+    if (player?.wins == null) {
+      return null;
+    }
+    const { wins, losses, gameWins, gameLosses } = player;
+    return (
+      <Chip
+        style={{ marginLeft: 10 }}
+        label={t('winLoss', { wins, losses, gameWins, gameLosses })}
+      />
+    )
   }
 
   const noSearchResults = searchedPlayers == null || searchedPlayers.length <= 0;
@@ -52,11 +80,14 @@ export default function SinglePlayerList(props) {
       />
       {
         noSearchResults ? <p>{t('no_players_in_search')}</p>
-        : searchedPlayers?.map((player) => (
+        : searchedPlayers.sort(sortingAlgo)?.map((player) => (
           <Card>
             <div className={classes.root}>
-              <div className={classes.playerNameRow}>
-                <h4>{player.name} {getValidLabel(showValid, player.valid)}</h4>
+              <div className={`${classes.playerNameRow} realign`}>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <h4>{player.name} {getValidLabel(showValid, player.valid)}</h4>
+                  {renderStats(player)}
+                </div>
                 <div>
                   <Button onClick={() => onPlayer(player.name)}>
                     {t("view_profile")}
