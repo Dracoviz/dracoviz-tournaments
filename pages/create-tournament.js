d9c4aa349a50d0b5c05b276b5513a00aade52c88
DeveloperKhan <adib.khan@gmail.com>
2023-11-05 09:23:58 -0800

    Fix salvation cup

--- pages/create-tournament.js
+++ pages/create-tournament.js
@@ -125,7 +125,7 @@ export default function CreateTournament() {
           <MenuItem value="Flash (Dojo Cup)">Flash (Dojo Cup)</MenuItem>
           <MenuItem value="NMFC (Midnight Cup)">NMFC (Midnight Cup)</MenuItem>
           <MenuItem value="Flash (Nature Cup)">Flash (Nature Cup)</MenuItem>
-          <MenuItem value="NMFC (Salvation Cup)">Flash (Salvation Cup)</MenuItem>
+          <MenuItem value="Flash (Salvation Cup)">NMFC (Salvation Cup)</MenuItem>
           <MenuItem value="Nimbus Cup">Nimbus Cup</MenuItem>
           <MenuItem value="BTW (Neutral Cup)">BTW (Neutral Cup)</MenuItem>
         </Select>
