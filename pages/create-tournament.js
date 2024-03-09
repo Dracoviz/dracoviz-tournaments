f5359e7275e90a72131644201e9aed556bc79541
DeveloperKhan <adib.khan@gmail.com>
2024-03-09 09:33:13 -0800

    Add spring cup

--- pages/create-tournament.js
+++ pages/create-tournament.js
@@ -178,6 +178,7 @@ export default function CreateTournament() {
           <MenuItem value="Evolution Cup">Evolution Cup</MenuItem>
           <MenuItem value="Dominion Cup">Dominion Cup</MenuItem>
           <MenuItem value="Zodiac Cup">Zodiac Cup</MenuItem>
+          <MenuItem value="Spring Cup">Spring Cup</MenuItem>
         </Select>
       </GridItem>
     ))
