39eab221be84db8cb35922681e798bf70252bb97
DeveloperKhan <adib.khan@gmail.com>
2024-04-10 23:37:20 +0200

    add cups

--- pages/create-tournament.js
+++ pages/create-tournament.js
@@ -184,6 +184,8 @@ export default function CreateTournament() {
           <MenuItem value="Uso Cup">Uso Cup</MenuItem>
           <MenuItem value="Rainstorm Cup">Rainstorm Cup</MenuItem>
           <MenuItem value="The Silver Citadel">The Silver Citadel</MenuItem>
+          <MenuItem value="PaoLaf Cup">PaoLaf Cup</MenuItem>
+          <MenuItem value="BTW (Mystical Fields)">BTW (Mystical Fields)</MenuItem>
         </Select>
       </GridItem>
     ))
