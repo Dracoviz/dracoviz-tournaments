8a24377ca1e07099b1e39a920a0080f381ed37a3
DeveloperKhan <adib.khan@gmail.com>
2024-06-11 23:03:10 -0700

    fix

--- pages/tournament/[id].js
+++ pages/tournament/[id].js
@@ -445,7 +445,7 @@ export default function Tournament() {
   }
 
   const renderExportButton = () => {
-    if (data == null || (data.state !== "POKEMON_VISIBLE" && !isHost)) {
+    if (data == null || (data.state !== "POKEMON_VISIBLE" && !data.isHost)) {
       return null
     }
     return (
