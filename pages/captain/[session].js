8afd07888a70b9978125cd43c7694ffc5b8ca9da
DeveloperKhan <adib.khan@gmail.com>
2024-09-16 22:34:47 -0700

    change matchup

--- pages/captain/[session].js
+++ pages/captain/[session].js
@@ -182,7 +182,7 @@ export default function Team() {
                 <GridItem xs={12} style={{ marginTop: 30 }}>
                   <Button
                     type="submit"
-                    disabled={isLoading || !isValid || submitting || !canEdit}
+                    disabled={isLoading || submitting || !canEdit}
                     fullWidth
                     style={{ marginBottom: 10 }}
                   >
